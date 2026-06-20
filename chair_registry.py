69f9513bbf082bb332a70855eddecd0b8519e2f6
Taisonsasa7 <TaisonSasa7@gmail.com>
2026-06-20 14:29:09 +0300

    Add new elemental thrones to chair registry

--- chair_registry.py
+++ chair_registry.py
@@ -25,3 +25,38 @@ ELEMENTAL_THRONES = {
 
 def get_throne_data(chair_id):
     return ELEMENTAL_THRONES.get(chair_id, None)
+# chair_registry.py
+
+ELEMENTAL_THRONES = {
+    # الكراسي الموجودة مسبقاً
+    "dragon_fire": {
+        "name": "عرش التنين الناري",
+        "entity_type": "dragon",
+        "gift_effect": "golden_fire_cloud",
+        "sound_effect": "dragon_fire_roar.mp3",
+        "movement_speed": 1.2
+    },
+    # الكراسي الجديدة (الفيل والطاووس)
+    "elephant_throne": {
+        "name": "عرش الفيل العظيم",
+        "entity_type": "elephant",
+        "gift_effect": "water_splash_gift",
+        "sound_effect": "elephant_trumpet.mp3",
+        "movement_speed": 0.5
+    },
+    "peacock_throne": {
+        "name": "عرش الطاووس الملكي",
+        "entity_type": "peacock",
+        "gift_effect": "feather_dance_gift",
+        "sound_effect": "peacock_cry.mp3",
+        "movement_speed": 0.7
+    },
+    # يمكنك إضافة أي كرسي جديد بنفس هذا التنسيق
+    "lion_throne": {
+        "name": "عرش الأسد الملكي",
+        "entity_type": "lion",
+        "gift_effect": "royal_roar_gift",
+        "sound_effect": "lion_roar.mp3",
+        "movement_speed": 0.8
+    }
+}
