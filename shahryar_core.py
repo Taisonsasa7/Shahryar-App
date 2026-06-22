f67830d50b7e14b60eab01e2c3be4f3b9e5e3864
Taisonsasa7 <TaisonSasa7@gmail.com>
2026-06-22 14:37:49 +0300

    Refactor ShahryarCore by removing ShahryarEconomy
    Removed ShahryarEconomy class and its related methods. Updated rooms structure to a list of dictionaries.

--- shahryar_core.py
+++ shahryar_core.py
@@ -1,33 +1,9 @@
-import json
-import os
-
-class ShahryarEconomy:
-    def _init_(self):
-        self.EXCHANGE_RATE = 10000
-
-    def calculate_distribution(self, amount):
-        return {
-            "total_diamonds": amount * self.EXCHANGE_RATE,
-            "admin": (amount * self.EXCHANGE_RATE) * 0.60,
-            "host": (amount * self.EXCHANGE_RATE) * 0.30
-        }
-
 class ShahryarCore:
     def _init_(self):
-        self.economy = ShahryarEconomy()
-        # تأكد أن هذه السطور تبدأ بـ 8 مسافات من يسار الصفحة
-        self.rooms = {
-            "gaming": "غرفة الألعاب",
-            "music": "غرفة الموسيقى",
-            "iran_scandal": "فضيحة إيران",
-            "throne": "مجلس العرش"
-        }
-
+        self.rooms = [{"name": "غرفة تجريبية"}]
+    
     def get_all_rooms(self):
-        return [{"id": k, "name": v} for k, v in self.rooms.items()]
+        return self.rooms
 
-    def trigger_event(self, amount):
-        return self.economy.calculate_distribution(amount)
-
-# هذا الكائن يجب أن يكون في نهاية الملف تماماً
+# إنشاء الكائن
 shahryar_system = ShahryarCore()
