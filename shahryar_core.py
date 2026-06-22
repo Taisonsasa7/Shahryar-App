543811ad53909ac4bc9ae1a22b4259eae4044edf
Taisonsasa7 <TaisonSasa7@gmail.com>
2026-06-22 15:10:03 +0300

    Refactor ShahryarCore class methods

--- shahryar_core.py
+++ shahryar_core.py
@@ -1,18 +1,11 @@
 class ShahryarCore:
     def _init_(self):
-        # تعريف الغرف كقائمة بسيطة
         self.rooms = [{"name": "غرفة الألعاب"}, {"name": "غرفة الموسيقى"}]
     
     def get_all_rooms(self):
-        # إرجاع القائمة مباشرة
         return self.rooms
 
     def calculate_money(self, amount):
-        return {
-            "إجمالي": amount,
-            "حصة الإدارة": amount * 0.60,
-            "حصة المضيف": amount * 0.30
-        }
+        return {"إجمالي": amount, "حصة الإدارة": amount * 0.60, "حصة المضيف": amount * 0.30}
 
-# إنشاء الكائن لاستخدامه في الملف الآخر
 shahryar_system = ShahryarCore()
