1e0d09f2860776bba20ce4421c1893953bdc7739
Taisonsasa7 <TaisonSasa7@gmail.com>
2026-06-22 14:43:24 +0300

    Modify room initialization and add money calculation method
    Updated the rooms list to include multiple room names and added a method to calculate money distribution.

--- shahryar_core.py
+++ shahryar_core.py
@@ -1,9 +1,18 @@
 class ShahryarCore:
     def _init_(self):
-        self.rooms = [{"name": "غرفة تجريبية"}]
+        # تعريف الغرف كقائمة من القواميس تحتوي على الاسم فقط
+        self.rooms = [{"name": "غرفة الألعاب"}, {"name": "غرفة الموسيقى"}]
     
     def get_all_rooms(self):
         return self.rooms
 
-# إنشاء الكائن
+    def calculate_money(self, amount):
+        # عملية حسابية بسيطة
+        return {
+            "إجمالي": amount,
+            "حصة الإدارة": amount * 0.60,
+            "حصة المضيف": amount * 0.30
+        }
+
+# إنشاء الكائن الموحد
 shahryar_system = ShahryarCore()
